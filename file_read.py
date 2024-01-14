5f6b13781728018c7f0790f5abf579f281ab74d0
mayayannnn <sakamoto-t.m@nifty.com>
2024-01-14 12:43:07 +0900

    .

--- file_read.py
+++ file_read.py
@@ -13,52 +13,15 @@ for i in list:
 for i in files:
     target = '.'
     idx = i.find(target)
-    r = i[idx:] 
-    K = r
+    k = i[idx:]
     year = i[0] + i[1] + i[2] + i[3]
-    if i[4] == "0":
-        if i[5] == "1":
-            month = "01"
-        elif i[5] == "2":
-            month = "02"
-
-        elif i[5] == "3":
-            month = "03"
-
-        elif i[5] == "4":
-            month = "04"
-
-        elif i[5] == "5":
-            month = "05"
-
-        elif i[5] == "6":
-            month = "06"
-
-        elif i[5] == "7":
-            month = "07"
-
-        elif i[5] == "8":
-            month = "08"
-
-        elif i[5] == "9":
-            month = "09"
-    if i[4] == "1":
-        if i[5] == "0":
-            month = "10"
-
-        elif i[5] == "1":
-            month = "11"
-
-        elif i[5] == "2":
-            month = "12"
-
-    time.append(year + month + K)
+    month = i[4] + i[5]
+    time.append(year + month + k)
 files.clear
-
 for i in time:
     target = '.'
     idx = i.find(target)
-    r = i[:idx] 
+    r = i[:idx]
     new_path = shutil.move(rf"file/folder1/{i}",rf"file/folder2/{r}")
     print(new_path)
 time.clear
